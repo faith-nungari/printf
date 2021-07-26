4f456daf1ec89a3683c57cfdeb12d8f056dd38c0
Faith <faykonye2@gmail.com>
2021-07-26 02:40:27 -0700

    Printf

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * _printf - produces output according to a format
+ *
+ * @format: string to print and count
+ *
+ * Return: number of charachters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int print_count = 0;
+	va_list arg_list;
+
+	if (format == NULL)
+		return (0);
+
+	va_start(arg_list, format);
+	print_count = parser(format, arg_list);
+        va_end(arg_list);
+
+	if (print_count)
+		return (print_count);
+
+	return (0);
+}
